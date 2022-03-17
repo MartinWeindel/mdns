072a08fe159e48bd3f40acfbe6024008f92966c0
Martin Weindel <martin.weindel@sap.com>
2022-03-17 18:46:16 +0100

    no log on closing client

--- client.go
+++ client.go
@@ -182,7 +182,7 @@ func (c *client) Close() error {
 		return nil
 	}
 
-	log.Printf("[INFO] mdns: Closing client %v", *c)
+	//log.Printf("[INFO] mdns: Closing client %v", *c)
 	close(c.closedCh)
 
 	if c.ipv4UnicastConn != nil {
